3a55735df0996ca1ba44944386536a8e131f7dad
Steve Martocci <smartocci@gmail.com>
2008-08-23 15:21:12 -0400

    added fixture loading via db:populate

---
+++ db/fixtures/seasons.rb
@@ -0,0 +1,4 @@
+Season.create_or_update(:id => 1, :name => "Season 1", :permalink => "season-1")
+Season.create_or_update(:id => 2, :name => "Season 2", :permalink => "season-2")
+Season.create_or_update(:id => 3, :name => "Season 3", :permalink => "season-3")
+Season.create_or_update(:id => 4, :name => "Season 4", :permalink => "season-4")
