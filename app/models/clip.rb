c3111e6dbe120498febfc2cc6884c536d1fbaf80
Steve Martocci <smartocci@gmail.com>
2008-08-23 15:21:30 -0400

    added script to upload data

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -6,4 +6,15 @@ class Clip < ActiveRecord::Base
   has_attachment :storage => :s3
   acts_as_list
   acts_as_taggable_on :tags
+  
+  def self.find_or_create_from_bucket(s_id, e_id, p_id, bucket, key)
+    clip = Clip.find_or_initialize_by_season_id_and_episode_id_and_position(:season_id => s_id.to_i, :episode_id => e_id.to_i, :position => p_id.to_i)
+    if clip.new_record?
+      s3_file = bucket[key]
+      clip.temp_data = s3_file.value
+      clip.content_type = "binary/octet-stream"
+      clip.filename = "s#{s_id}e#{e_id}-c#{p_id}.flv"
+      clip.save
+    end
+  end
 end
